5fcdf15dbd6c0be9a01d9b9004c6774a99206ee2
riyabelle25 <riya_e@es.iitr.ac.in>
2020-12-07 14:10:18 +0530

    init project

---
+++ octo/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static 
+from django.conf import settings 
+from scripts import views
+
+urlpatterns = [
+    path('', views.current_datetime, name='home'),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

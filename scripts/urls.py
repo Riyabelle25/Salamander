1e0c5a38472232cdcf4e1563eca17cd0bdb540df
guptaprakhariitr <prakharpguptag@gmail.com>
2020-12-11 02:06:42 +0530

    script Ready

--- scripts/urls.py
+++ scripts/urls.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('scrapper',views.scrapper , name='Scrapping Followers'),
+    path('scrapper/',views.scrapper , name='Scrapping Followers'),
 ]
--- scripts/views.py
+++ scripts/views.py
@@ -4,6 +4,7 @@ import datetime
 import firebase_admin
 import google.cloud
 import pandas as pd
+import json
 import numpy as np
 from scipy.sparse import csr_matrix
 from selenium import webdriver
@@ -18,6 +19,7 @@ from selenium.common.exceptions import StaleElementReferenceException
 from selenium.common.exceptions import TimeoutException
 import time
 import os
+from urllib.request import urlopen
 import datetime
 from webdriver_manager.chrome import ChromeDriverManager
 
@@ -56,19 +58,11 @@ def Results_toFirestore():
 
 
 def calculate_recommendations(request):
-    """Responds to any HTTP request.
-    Args:
-        request (flask.Request): HTTP request object.
-    Returns:
-        The response text or any set of values that can be turned into a
-        Response object using
-        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
-    """
     col_ref = store.collection('users')
     try:
         docs = col_ref.get()
         for doc in docs:
-            tmp = 'users/'+ str(doc.id) + '/followers'
+            tmp = 'users/'+ str(doc.id) + '/following'
             print(tmp)
             store.collection(tmp).document("HJqFZWxxwnh0qxm8GiFF").set(Results_toFirestore())
 
@@ -82,33 +76,60 @@ def calculate_recommendations(request):
 def scrapper(request):
     count = 100  # number of profiles you want to scrap
     account = "salamandar_nemesis"  # account from
-    page = "followers"  # from following or followers
+    page = "following"  # from following or followers
 
     yourusername = "salamandar_nemesis" #your Instagram username
-    yourpassword = "*******"  #your Instagram password
-
-
-#for proxy i recommend 4G mobile proxy: http://www.virtnumber.com/mobile-proxy-4g.php
-#PROXY = "http://84.52.54.2:8011" # IP:PORT or HOST:PORT
-#options.add_argument('--proxy-server=%s' % PROXY)
+    yourpassword = "prakhar123"  #your Instagram password
 
     options = webdriver.ChromeOptions()
     options.add_argument('--ignore-certificate-errors')
     options.add_argument('--user-agent="Mozilla/5.0 (iPhone; CPU iPhone OS 12_1_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/16D57"')
-
-    driver = webdriver.Chrome(executable_path='/mnt/c/Users/Lenovo/Desktop/Salamander/scripts/chromedriver.exe')
-
+    try:
+        driver = webdriver.Chrome(executable_path='./scripts/chromedriver')
+    except:
+        driver = webdriver.Chrome(executable_path='./scripts/chromedriver.exe')       
     driver.get('https://www.instagram.com/accounts/login/')
-    sleep(3)
+    sleep(2)
     username_input = driver.find_element_by_css_selector("input[name='username']")
     password_input = driver.find_element_by_css_selector("input[name='password']")
+    attack = 'prakhar__gupta__'
     username_input.send_keys(yourusername)
     password_input.send_keys(yourpassword)
     login_button = driver.find_element_by_xpath("//button[@type='submit']")
     login_button.click()
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(.,'Not Now')]"))).click()
-    sleep(3)
-          
+    sleep(1)
+    
+    dirname = os.path.dirname(os.path.abspath(__file__))
+    csvfilename = os.path.join(dirname, account + "-" + page + ".txt")
+    f = open(csvfilename,'w')
+    
+    driver.get('https://www.instagram.com/%s' % attack)
+    sleep(2) 
+    driver.find_element_by_xpath('//a[contains(@href, "%s")]' % page).click()
+    scr2 = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a')
+    sleep(2)
+    text1 = scr2.text
+    print(text1)
+    x = datetime.datetime.now()
+    print(x)
+
+    for i in range(1,50):
+        try:
+            scr1 = driver.find_element_by_xpath('/html/body/div[5]/div/div/div[2]/ul/div/li[%s]' % i)
+            driver.execute_script("arguments[0].scrollIntoView();", scr1)
+        except:
+            break
+        sleep(0.1)
+        text = scr1.text
+        list = text.encode('utf-8').split()
+        file_exists = os.path.isfile(csvfilename)
+        print('{};{}'.format(i, str(list[0]).split('\'')[1]))
+        f.write((str(list[0]).split('\'')[1]) + "\r\n")
+        if i == (count-1):
+            print(x)
+    # account
+    faccount = open("account",'w')
     driver.get('https://www.instagram.com/%s' % account)
     sleep(2) 
     driver.find_element_by_xpath('//a[contains(@href, "%s")]' % page).click()
@@ -119,19 +140,67 @@ def scrapper(request):
     x = datetime.datetime.now()
     print(x)
 
-    for i in range(1,5):
-        scr1 = driver.find_element_by_xpath('/html/body/div[5]/div/div/div[2]/ul/div/li[%s]' % i)
-        driver.execute_script("arguments[0].scrollIntoView();", scr1)
-        sleep(1)
+    for i in range(1,15):
+        try:
+            scr1 = driver.find_element_by_xpath('/html/body/div[5]/div/div/div[2]/ul/div/li[%s]' % i)
+            driver.execute_script("arguments[0].scrollIntoView();", scr1)
+        except:
+            break
+        sleep(0.3)
         text = scr1.text
         list = text.encode('utf-8').split()
-        dirname = os.path.dirname(os.path.abspath(__file__))
-        csvfilename = os.path.join(dirname, account + "-" + page + ".txt")
-        file_exists = os.path.isfile(csvfilename)
-        f = open(csvfilename,'a')
-        f.write(str(list[0]) + "\r\n")
-        f.close()
-        print('{};{}'.format(i, list[0]))
+        file_exists = os.path.isfile("account")
+        print('{};{}'.format(i, str(list[0]).split('\'')[1]))
+        faccount.write((str(list[0]).split('\'')[1]) + "\r\n")
         if i == (count-1):
             print(x)
-    return HttpResponse('Done!')  
+    faccount.close()
+    #
+    faccount=open("account",'r')
+    for x in faccount:
+        driver.get('https://www.instagram.com/%s' % x.split('\n')[0])
+        sleep(2) 
+        driver.find_element_by_xpath('//a[contains(@href, "%s")]' % page).click()
+        scr2 = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a')
+        sleep(2)
+        text1 = scr2.text
+        print(text1)
+        x = datetime.datetime.now()
+        print(x)
+
+        for i in range(1,40):
+            try:
+                scr1 = driver.find_element_by_xpath('/html/body/div[5]/div/div/div[2]/ul/div/li[%s]' % i)
+                driver.execute_script("arguments[0].scrollIntoView();", scr1)
+            except:
+                break
+            sleep(1)
+            text = scr1.text
+            list = text.encode('utf-8').split()
+            file_exists = os.path.isfile(csvfilename)
+            print('{};{}'.format(i, str(list[0]).split('\'')[1]))
+            f.write((str(list[0]).split('\'')[1]) + "\r\n")
+            if i == (count-1):
+                print(x)
+    #
+    f.close()
+    f = open(csvfilename,'r')
+    st=f.read()
+    f.close()
+    f = open(csvfilename,'r')
+    flock = open("abc",'w')
+    for x in f:
+        link = "https://www.instagram.com/"+(x.split('\n')[0])+"/?__a=1"
+        print(link)
+        webUrl = urlopen(link).read()
+        flock.write(webUrl.decode("utf-8"))
+        flock.write("\n")
+    print("ab")
+    f.close()
+    flock.close()
+    f = open("abc",'r')
+    for x in f:
+        res = json.loads(x)
+        print(str(res["graphql"]["user"]["is_private"]))
+    f.close()
+    return HttpResponse("st")
